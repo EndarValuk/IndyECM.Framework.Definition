ddc34f828be30861838b80f30f7914adcdeed066
Dmitry Besedin <endarvaluk@gmail.com>
2018-05-09 23:06:44 +0500

    [+] Supported API, IAM and DB enums

---
+++ Enumeration/Services/ApiResponseType.cs
@@ -0,0 +1,29 @@
+﻿namespace IndyECM.Framework.Definition.Enumeration.Services
+{
+  ///<summary>
+  /// API host supported response types enumeration
+  ///</summary>
+  public enum ApiResponseType
+  {
+    ///<summary>
+    /// API will serve response as <see>application/json</see>
+    ///</summary>
+    Json,
+    ///<summary>
+    /// API will serve response as <see>application/xml</see>
+    ///</summary>
+    Xml,
+    ///<summary>
+    /// API will serve response as <see>application/bson</see>
+    ///</summary>
+    Bson,
+    ///<summary>
+    /// API will serve response as <see>application/msgpack</see>
+    ///</summary>
+    MsgPack,
+    ///<summary>
+    /// API will serve response as <see>application/x-protobuf</see>
+    ///</summary>
+    ProtoBuf
+  }
+}
---
+++ Enumeration/Services/IamServiceType.cs
@@ -0,0 +1,29 @@
+﻿namespace IndyECM.Framework.Definition.Enumeration.Services
+{
+  ///<summary>
+  /// Identity and access management supported service types enumeration
+  ///</summary>
+  public enum IamServiceType
+  {
+    ///<summary>
+    /// Always use anonymous access
+    ///</summary>
+    None,
+    ///<summary>
+    /// ActiveDirectory based
+    ///</summary>
+    ActiveDirectory,
+    ///<summary>
+    /// LDAP based
+    ///</summary>
+    LDAP,
+    ///<summary>
+    /// DataStorage based
+    ///</summary>
+    DataStorage,
+    ///<summary>
+    /// OAuth based
+    ///</summary>
+    OAuth,
+  }
+}
